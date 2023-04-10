c3eaa25408fa0ac1e831ab85fd59cb8d258aa394
tillerload <tyler.george.work@gmail.com>
2023-04-10 16:23:52 -0230

    deleted everything

--- hamMenu.js
+++ hamMenu.js
@@ -1,7 +1,8 @@
-const dropButton = document.querySelector('dropDownButton');
+const dropButton = document.querySelector('.dropDownButton');
+const dropDown = document.querySelector('.dropDown')
 
-dropButton.addEventListener('click',)
+dropButton.addEventListener('click', () => {
+  console.log('hey')
+  dropDown.classList.toggle.displayBlock
+})
 
-const dropDown = () => {
-  
-}
